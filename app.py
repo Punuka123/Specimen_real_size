254819a83abbed00075d44c08e09e62c4a435d89
Punuka123 <punuka5678@gmail.com>
2025-04-11 23:22:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -53,4 +54,5 @@ def index():
 
 if __name__ == "__main__":
     setup_database()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
